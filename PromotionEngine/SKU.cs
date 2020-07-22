9e1ec9310be6f497742858242ce9f6af53798014
Sai Manoj Yelchuri <saimanoj.yelchuri@bt.com>
2020-07-22 20:33:32 +0530

    initialized the basket

--- PromotionEngine/SKU.cs
+++ PromotionEngine/SKU.cs
@@ -8,5 +8,28 @@ namespace PromotionEngine
 {
     class SKU
     {
+        private List<char> items = new List<char>();
+        Dictionary<char, double> itemByPrice = new Dictionary<char, double>();
+
+        public SKU(Dictionary<char, double> itemsByPrice)
+        {
+            this.itemByPrice = itemsByPrice;
+        }
+
+        public void AddToSKU(char item)
+        {
+            items.Add(item);
+        }
+
+        public List<char> GetSKU()
+        {
+            return items;
+        }
+
+        public Dictionary<char, double> GetItemByPrice()
+        {
+            return itemByPrice;
+        }
     }
+
 }
